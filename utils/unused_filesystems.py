1f728df3f8c8b837d4054ae824d58f703284dc29
Augadh Verma <augadhverma@gmail.com>
2024-05-29 16:59:00 +0530

    Added  more checks

--- utils/unused_filesystems.py
+++ utils/unused_filesystems.py
@@ -37,8 +37,10 @@ def ensure_cramfs_disabled():
     pretty_print("[1.1.1.1] Ensure mounting of cramfs filesystems is disabled (Scored)")
     print()
     
-    modprobe_command = 'modprobe -n -v cramfs'
-    lsmod_command = 'lsmod | grep cramfs'
+    filesystem = 'cramfs'
+
+    modprobe_command = f'modprobe -n -v {filesystem}'
+    lsmod_command = f'lsmod | grep {filesystem}'
 
     print(f"Running command: {modprobe_command}")
     modprobe_result = subprocess.run(modprobe_command, shell=True, capture_output=True, text=True)
@@ -60,22 +62,189 @@ def ensure_cramfs_disabled():
     else:
         pretty_underline(lsmod_result.stdout)
 
-    expected_output_modprobe = """
-insmod /lib/modules/6.5.O-35-generic/kernel/drivers/mtd/mtd.ko
-insmod /lib/modules/6.5.O-35-generic/kernel/fs/cramfs/cramfs.ko
-"""
+    expected_output_modprobe = "insmod /lib/modules/6.5.O-35-generic/kernel/fs/cramfs/cramfs.ko"
     
     # insmod /lib/modules/6.5.O-35-generic/kernel/drivers/mtd/mtd.ko
     # insmod /lib/modules/6.5.O-35-generic/kernel/fs/cramfs/cramfs.ko
 
     modprobe_disabled = expected_output_modprobe in modprobe_result.stdout
-    lsmod_disabled = lsmod_result.stdout.strip() == ""
+    lsmod_disabled = not lsmod_result.stdout.strip()
 
     if modprobe_disabled and lsmod_disabled:
-        print("Cramfs filesystem mounting is disabled")
+        print(f"{filesystem} filesystem mounting is disabled")
     else:
-        print("Cramfs filesystem mounting is not properly disabled.")
+        print(f"{filesystem} filesystem mounting is not properly disabled.")
     print()
+
+
+def ensure_freevxfs_disabled():
+    """
+    Profile Applicability:
+    ----------------------
+    - Level 1 - Server
+    - Level 1 - Workstation
+
+    Description:
+    ------------
+    The freevxfs filesystem type is a free version of the Veritas type filesystem. This is the
+    primary filesystem type for HP-UX operating systems.
+
+    Rationale:
+    ----------
+    Removing support for unneeded filesystem types reduces the local attack surface of the
+    system. If this filesystem type is not needed, disable it.
+    """
+    pretty_print("[1.1.1.2] Ensure mounting of freevxfs filesystems is disabled (Scored)")
+    print()
+    
+    filesystem = 'freevxfs'
+
+    modprobe_command = f'modprobe -n -v {filesystem}'
+    lsmod_command = f'lsmod | grep {filesystem}'
+
+    print(f"Running command: {modprobe_command}")
+    modprobe_result = subprocess.run(modprobe_command, shell=True, capture_output=True, text=True)
+    print(modprobe_result.stdout)
+    if modprobe_result.stderr:
+        print("Error:")
+        print(modprobe_result.stderr.strip())
+        pretty_underline(modprobe_result.stderr, "-")
+    else:
+        pretty_underline(modprobe_result.stdout)
+
+    print(f"Running command: {lsmod_command}")
+    lsmod_result = subprocess.run(lsmod_command, shell=True, capture_output=True, text=True)
+    print(lsmod_result.stdout)
+    if lsmod_result.stderr:
+        print("Error:")
+        print(lsmod_result.stderr.strip())
+        pretty_underline(lsmod_result.stderr, "-")
+    else:
+        pretty_underline(lsmod_result.stdout)
+
+    expected_output_modprobe = "insmod /lib/modules/6.5.0-35-generic/kernel/fs/freevxfs/freevxfs.ko"
+
+    modprobe_disabled = expected_output_modprobe in modprobe_result.stdout
+    lsmod_disabled = not lsmod_result.stdout.strip()
+
+    if modprobe_disabled and lsmod_disabled:
+        print(f"{filesystem} filesystem mounting is disabled")
+    else:
+        print(f"{filesystem} filesystem mounting is not properly disabled.")
+    print()
+
+def ensure_jffs2_disabled():
+    """
+    Profile Applicability:
+    ----------------------
+    - Level 1 - Server
+    - Level 1 - Workstation
+
+    Description:
+    ------------
+    The jffs2 (journaling flash filesystem 2) filesystem type is a log-structured filesystem used
+    in flash memory devices.
+
+    Rationale:
+    ----------
+    Removing support for unneeded filesystem types reduces the local attack surface of the
+    system. If this filesystem type is not needed, disable it.
+    """
+    pretty_print("[1.1.1.3] Ensure mounting of jffs2 filesystems is disabled (Scored)")
+    print()
+    
+    filesystem = 'jffs2'
+
+    modprobe_command = f'modprobe -n -v {filesystem}'
+    lsmod_command = f'lsmod | grep {filesystem}'
+
+    print(f"Running command: {modprobe_command}")
+    modprobe_result = subprocess.run(modprobe_command, shell=True, capture_output=True, text=True)
+    print(modprobe_result.stdout)
+    if modprobe_result.stderr:
+        print("Error:")
+        print(modprobe_result.stderr.strip())
+        pretty_underline(modprobe_result.stderr, "-")
+    else:
+        pretty_underline(modprobe_result.stdout)
+
+    print(f"Running command: {lsmod_command}")
+    lsmod_result = subprocess.run(lsmod_command, shell=True, capture_output=True, text=True)
+    print(lsmod_result.stdout)
+    if lsmod_result.stderr:
+        print("Error:")
+        print(lsmod_result.stderr.strip())
+        pretty_underline(lsmod_result.stderr, "-")
+    else:
+        pretty_underline(lsmod_result.stdout)
+
+    expected_output_modprobe = "insmod /lib/modules/6.5.O-35-generic/kernel/fs/jffs2/jffs2.ko"
+
+    modprobe_disabled = expected_output_modprobe in modprobe_result.stdout
+    lsmod_disabled = not lsmod_result.stdout.strip()
+
+    if modprobe_disabled and lsmod_disabled:
+        print(f"{filesystem} filesystem mounting is disabled")
+    else:
+        print(f"{filesystem} filesystem mounting is not properly disabled.")
+    print()
+
+def ensure_hfs_disabled():
+    """
+    Profile Applicability:
+    ----------------------
+    - Level 1 - Server
+    - Level 1 - Workstation
+
+    Description:
+    ------------
+    The jffs2 (journaling flash filesystem 2) filesystem type is a log-structured filesystem used
+    in flash memory devices.
+
+    Rationale:
+    ----------
+    Removing support for unneeded filesystem types reduces the local attack surface of the
+    system. If this filesystem type is not needed, disable it.
+    """
+    pretty_print("[1.1.1.4] Ensure mounting of hfs filesystems is disabled (Scored)")
+    print()
+    
+    filesystem = 'hfs'
+
+    modprobe_command = f'modprobe -n -v {filesystem}'
+    lsmod_command = f'lsmod | grep {filesystem}'
+
+    print(f"Running command: {modprobe_command}")
+    modprobe_result = subprocess.run(modprobe_command, shell=True, capture_output=True, text=True)
+    print(modprobe_result.stdout)
+    if modprobe_result.stderr:
+        print("Error:")
+        print(modprobe_result.stderr.strip())
+        pretty_underline(modprobe_result.stderr, "-")
+    else:
+        pretty_underline(modprobe_result.stdout)
+
+    print(f"Running command: {lsmod_command}")
+    lsmod_result = subprocess.run(lsmod_command, shell=True, capture_output=True, text=True)
+    print(lsmod_result.stdout)
+    if lsmod_result.stderr:
+        print("Error:")
+        print(lsmod_result.stderr.strip())
+        pretty_underline(lsmod_result.stderr, "-")
+    else:
+        pretty_underline(lsmod_result.stdout)
+
+    expected_output_modprobe = "insmod /lib/modules/6.5.O-35-generic/kernel/fs/hfs/hfs.ko"
+
+    modprobe_disabled = expected_output_modprobe in modprobe_result.stdout
+    lsmod_disabled = not lsmod_result.stdout.strip()
+
+    if modprobe_disabled and lsmod_disabled:
+        print(f"{filesystem} filesystem mounting is disabled")
+    else:
+        print(f"{filesystem} filesystem mounting is not properly disabled.")
+    print()
+
 
 def run():
     pretty_print("[1.1] Filesystem Configuration", upper_underline=True)
